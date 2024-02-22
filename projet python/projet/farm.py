f7bb02876a8c780118f404c7f690a276faef0194
Xavier Viel <Xavier.s.viel@gmail.com>
2024-02-22 15:31:27 -0500

    routes

--- projet python/projet/farm.py
+++ projet python/projet/farm.py
@@ -1,1 +1,28 @@
-import flask from Flask, test
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/farm/add")
+
+@app.route("/farm/modify")
+
+@app.route("/farm/remove")
+
+@app.route("/farm/status")
+
+@app.route("/builder/add")
+
+@app.route("/builder/modify")
+
+@app.route("/builder/remove")
+
+@app.route("/builder/assign")
+
+
+app.run()
