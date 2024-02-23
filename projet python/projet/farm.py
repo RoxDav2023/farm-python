b8a74fc6384d49d29dab6d50169ff784bea21054
Xavier Viel <Xavier.s.viel@gmail.com>
2024-02-22 22:53:54 -0500

    addfarms

--- projet python/projet/farm.py
+++ projet python/projet/farm.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask,redirect, render_template
+from flask import request
 import json
 
 app = Flask(__name__)
@@ -15,6 +16,36 @@ def fadd():
     return render_template('addfarm.html')
 
 @app.route("/farm/add", methods=['POST'])
+def faddPOST():
+    
+    farms = json.load(open('projet python\\gestion_taches_v01\\gestion_taches\\taches.json'))
+
+    # creation d'un nouveau todo
+    newfarm =  {}
+    newfarm['title'] = request.form['title']
+    #gestin des employes assignes
+    if (request.form.get['employee'] == json.load(open('projet python\\gestion_taches_v01\\gestion_taches\\employes.json'))):
+        newfarm['employee'] = {request.form.get['employee']}
+    else:
+        newfarm['employee'] = None
+    # la gestion du done est un peu plus compliquee...
+    if(request.form.get('done')):
+        newfarm['statut'] = 'done'
+        newfarm['employee'] = None
+    elif(request.form.get('in Progress')):    
+        newfarm['statut'] = 'in Progress'
+    else:
+        newfarm['statut'] = 'unnasigned'
+    newfarm['description'] = request.form.get['descrition']
+
+    # on lui donne un id unique : le plus grand id + 1
+    newfarm['id'] = (max(farms, key = lambda x: x['id'])['id'])+1
+    farms.append(newfarm)
+
+    # on ecrase le fichier avec la liste mise a jour
+    json.dump(farms, open('projet python\\gestion_taches_v01\\gestion_taches\\taches.json', 'w'))
+
+    return redirect('/todos')
 
 @app.route("/farm/modify?<int:id>", methods=['GET'])
 
