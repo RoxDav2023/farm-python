4b088c14ee89efa4efab17366bea5a72b56dbff6
danpoi01 <149626057+danpoi01@users.noreply.github.com>
2024-03-26 14:44:09 -0300

    meal generator

--- projet js/js/monjs.js
+++ projet js/js/monjs.js
@@ -39,7 +39,9 @@ get_meal_btn.addEventListener("click", () => {
     });
 });
 
+
 // create meal function
+
 const createMeal = meal => {
     const ingredients = [];
 
@@ -62,6 +64,35 @@ const createMeal = meal => {
                 ${
                     meal.strCategory
                     ? `<p><strong>Categorie:</strong> ${meal.strCategory}</p>`
-                    : " "
+                    : ""
                 }
+                $${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : " "}
+                ${
+                    meal.strTags
+                        ?`<p><strong>Tags:</strong> ${meal.strTags
+                            .split(",")
+                            .join(", ")}</p>`
+                        : ""
+                }
+                <h5>Ingredients:</h5>
+                <ul>
+                    ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join("")}
+                </ul>
+            </div>
+            ${
+                meal.strYoutube
+                    ? `
+            <div class="row">
+                <h5>Video recette</h5>
+                <div class="videoWrapper">
+                    <iframe width="420" height="315"
+                    src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}"
+                    </iframe>
+                </div>
+            </div>`
+                : ""
+            }
+        `;
+
+        meal_container.innerHTML = newInnerHTML;
 };
